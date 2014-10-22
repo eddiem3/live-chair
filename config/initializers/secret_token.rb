43a3d09b3f1b9ddce3082d53588c740c07dcaee0
Eddie Massey III <eem33@case.edu>
2014-10-21 23:33:53 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LiveChair::Application.config.secret_key_base = '751b722db6716c2f870ee535258bd014b0aefd565b83314fe6072151ea167aa0c1437df4e5369c365ef3c654e0a6d6f2c15816a90232a5ad698a790491cb053f'
