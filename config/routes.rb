2eca40bee7bfafff88c4d3e8e770c78a19b72c56
Ifeanyi <ikalu13@gmail.com>
2014-11-01 13:53:25 -0400

    add profile show page

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ LiveChair::Application.routes.draw do
   end
   resource :session, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create]
-  resources :profiles, only: [:new, :create]
+  resources :profiles, only: [:new, :create, :show]
 
   namespace :api, defaults: {format: 'json'} do
     namespace :v1 do
