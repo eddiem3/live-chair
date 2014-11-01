48fc04a5c17c445d6cab668c434c5c0a1f8a3c8b
Sakshyam Dahal <dahalsakshyam@gmail.com>
2014-11-01 13:55:18 -0400

    modify reviews_controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,8 +1,10 @@
 class ReviewsController < ApplicationController
 
 	def create
-		@current_profile = Profile.find(params[:review][:profile_id])
-		@review = @current_profile.reviews.build(review_params)
+		# @current_profile = Profile.find(params[:review][:profile_id])
+		# @review = @current_profile.reviews.build(review_params)
+		
+		@review = Review.new(review_params)
 		if @review.save
 			flash[:success] = "Review created"
 			redirect_to root_url
@@ -20,6 +22,13 @@ class ReviewsController < ApplicationController
 	private
 
 	def review_params
-		params.require(:review).permit(:comment, :rating, :user_id)
+		params.require(:review).permit(:comment, :rating, :user_id).
+		merge(profile: profile_id)
 	end
 end
+
+# def create
+# 	@profile = Profile.find(params[:profile_id])
+# 	@review = @profile.reviews.create(review_params)
+
+
